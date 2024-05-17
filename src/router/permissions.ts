4dd8cb63185b3f8ed7234ede112e68971420bb7f
dzqjk <wyt18528323960@outlook.com>
2024-05-17 21:32:18 +0800

    使用路由进行拼单是否登录

--- src/router/permissions.ts
+++ src/router/permissions.ts
@@ -5,6 +5,15 @@ import router from '@/router/router'
 import NProgress from 'nprogress'
 // 引入进度条样式
 import 'nprogress/nprogress.css'
+// 引入用户仓库
+import pinia from '@/stores'
+import useUserStore from '@/stores/modules/user'
+// 去除加载loading动画
+NProgress.configure({ showSpinner: false })
+// 用户仓库
+let userStore = useUserStore(pinia)
+// 存储用户未登录可以访问的路由路径
+let whiteList = ['/home']
 // 去除加载loading动画
 NProgress.configure({ showSpinner: false })
 // 配置全局守卫
@@ -13,7 +22,18 @@ router.beforeEach((to, from, next) => {
   NProgress.start()
   // 动态设置网页标题
   document.title = `${to.meta.title}`
-  next()
+  // 判断用户是否登录
+  const uuid = userStore.userInfo.uuid
+  if (uuid) {
+    next()
+  } else {
+    if (whiteList.includes(to.path)) {
+      next()
+    } else {
+      userStore.visible = true
+      next({ path: '/home', query: { redirect: to.fullPath } })
+    }
+  }
 })
 
 // 后置守卫
